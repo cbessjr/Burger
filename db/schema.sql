6c0ef4b3012ede94243e736aff65accc14f49bc9
Ender <cbessjr@gmail.com>
2019-01-27 20:23:08 -0700

    012719 0822 Update MVC model and active code. Error on the /GET

--- db/schema.sql
+++ db/schema.sql
@@ -22,8 +22,3 @@ PRIMARY KEY(id)
 USE burgers_db;
 SELECT * FROM burgers;
 
---Insert initial data into the database
-USE burgers_db;
-INSERT INTO burgers (burger_name, devoured) VALUES ("hamburger", FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ("cheese hamburger", FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ("double cheese hamburger", FALSE);
